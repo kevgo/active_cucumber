d1b0a0400381a89b808e5bdc2382368235ffb98a
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-04 09:44:02 -0600

    eueu

--- lib/active_cucumber.rb
+++ lib/active_cucumber.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "active_record"
 require "active_cucumber/cucumparer"
 require "active_cucumber/cucumberator"
 require "active_cucumber/active_record_builder"
