7d4d63fea0bebd940102b7f4296ed788e0d201b1
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-04 09:41:33 -0600

    Validate arguments

--- lib/active_cucumber.rb
+++ lib/active_cucumber.rb
@@ -10,6 +10,8 @@ module ActiveCucumber
   # Returns the attributes to create an instance of the given ActiveRecord class
   # that matches the given vertical Cucumber table
   def self.attributes_for(activerecord_class, cucumber_table, context: {})
+    validate_activerecord_class!(activerecord_class)
+    validate_context!(context)
     builder = ActiveRecordBuilder.new activerecord_class, context
     builder.attributes_for ActiveCucumber.vertical_table(cucumber_table)
   end
@@ -17,6 +19,8 @@ module ActiveCucumber
   # Creates entries of the given ActiveRecord class
   # specified by the given horizontal Cucumber table
   def self.create_many(activerecord_class, cucumber_table, context: {})
+    validate_activerecord_class!(activerecord_class)
+    validate_context!(context)
     builder = ActiveRecordBuilder.new activerecord_class, context
     builder.create_many ActiveCucumber.horizontal_table(cucumber_table)
   end
@@ -24,6 +28,8 @@ module ActiveCucumber
   # Creates an entry of the given ActiveRecord class
   # specified by the given vertical Cucumber table
   def self.create_one(activerecord_class, cucumber_table, context: {})
+    validate_activerecord_class!(activerecord_class)
+    validate_context!(context)
     builder = ActiveRecordBuilder.new activerecord_class, context
     builder.create_record ActiveCucumber.vertical_table(cucumber_table)
   end
@@ -33,12 +39,16 @@ module ActiveCucumber
   #
   # Sorts records by creation date.
   def self.diff_all!(clazz, cucumber_table, context: {})
+    validate_activerecord_class_or_collection!(clazz)
+    validate_context!(context)
     cucumparer = Cucumparer.new clazz, cucumber_table, context
     cucumber_table.diff! cucumparer.to_horizontal_table
   end
 
   # Verifies that the given object matches the given vertical Cucumber table
   def self.diff_one!(object, cucumber_table, context: {})
+    validate_activerecord_instance!(object)
+    validate_context!(context)
     cucumparer = Cucumparer.new object.class, cucumber_table, context
     cucumber_table.diff! cucumparer.to_vertical_table(object)
   end
@@ -51,5 +61,19 @@ module ActiveCucumber
   # Returns the given vertical Cucumber table in standardized format
   def self.vertical_table(table)
     table.rows_hash
+  end
+
+  # @api private
+  def self.validate_activerecord_class!(klass)
+    return if klass.is_a?(Class) && klass < ActiveRecord::Base
+
+    raise TypeError, "Expected an ActiveRecord class, got #{klass.inspect}"
+  end
+
+  # @api private
+  def self.validate_context!(context)
+    return if context.is_a?(Hash)
+
+    raise TypeError, "Expected context to be a Hash, got #{context.class}"
   end
 end
