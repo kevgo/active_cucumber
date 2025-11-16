0a55352e5cd3dc637247a73c5eecc478aa533733
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-16 07:11:26 -0600

    Rename clazz to activerecord_class (#52)

--- lib/active_cucumber/active_record_builder.rb
+++ lib/active_cucumber/active_record_builder.rb
@@ -4,7 +4,7 @@ module ActiveCucumber
   # Creates ActiveRecord entries with data from given Cucumber tables.
   class ActiveRecordBuilder
     def initialize(activerecord_class, context)
-      @clazz = activerecord_class
+      @activerecord_class = activerecord_class
       @creator_class = creator_class
       @context = context
     end
@@ -22,7 +22,7 @@ module ActiveCucumber
     def create_record(attributes)
       creator = @creator_class.new(attributes, @context)
       factorybot_attributes = creator.factorybot_attributes
-      factory_name = @clazz.name.underscore.to_sym
+      factory_name = @activerecord_class.name.underscore.to_sym
       create_with_factory(factory_name, factorybot_attributes, attributes)
     end
 
@@ -32,13 +32,14 @@ module ActiveCucumber
     def create_with_factory(factory_name, factorybot_attributes, attributes)
       FactoryBot.create(factory_name, factorybot_attributes)
     rescue ActiveRecord::RecordInvalid => e
-      record = e.record || @clazz.new
+      record = e.record || @activerecord_class.new
       raise ActiveRecord::RecordInvalid.new(
         record,
-        "Failed to create #{@clazz.name} with attributes #{attributes.inspect}: #{e.message}"
+        "Failed to create #{@activerecord_class.name} " \
+        "with attributes #{attributes.inspect}: #{e.message}"
       )
     rescue ArgumentError => e
-      raise ArgumentError, "Failed to create #{@clazz.name}: #{e.message}. " \
+      raise ArgumentError, "Failed to create #{@activerecord_class.name}: #{e.message}. " \
                            "Make sure a FactoryBot factory is defined for :#{factory_name}"
     end
 
@@ -51,7 +52,7 @@ module ActiveCucumber
 
     # Returns the name of the Creator subclass to be used by this ActiveRecordBuilder instance.
     def creator_class_name
-      "#{@clazz.name}Creator"
+      "#{@activerecord_class.name}Creator"
     end
   end
 end
