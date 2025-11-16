3f3bfcb74de9b5cf1cc8caf6ea87a99339273a36
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-16 07:18:16 -0600

    Use memoization better (#55)

--- lib/active_cucumber/active_record_builder.rb
+++ lib/active_cucumber/active_record_builder.rb
@@ -5,12 +5,11 @@ module ActiveCucumber
   class ActiveRecordBuilder
     def initialize(activerecord_class, context)
       @activerecord_class = activerecord_class
-      @creator_class = creator_class
       @context = context
     end
 
     def attributes_for(attributes)
-      @creator_class.new(attributes, @context).factorybot_attributes
+      creator_class.new(attributes, @context).factorybot_attributes
     end
 
     # Creates all entries in the given horizontal table hash
@@ -20,7 +19,7 @@ module ActiveCucumber
 
     # Creates a new record with the given attributes in the database
     def create_record(attributes)
-      creator = @creator_class.new(attributes, @context)
+      creator = creator_class.new(attributes, @context)
       factorybot_attributes = creator.factorybot_attributes
       factory_name = @activerecord_class.name.underscore.to_sym
       create_with_factory(factory_name, factorybot_attributes, attributes)
@@ -45,9 +44,11 @@ module ActiveCucumber
 
     # Returns the Creator subclass to be used by this ActiveRecordBuilder instance.
     def creator_class
-      creator_class_name.constantize
-    rescue NameError
-      Creator
+      @creator_class ||= begin
+        creator_class_name.constantize
+      rescue NameError
+        Creator
+      end
     end
 
     # Returns the name of the Creator subclass to be used by this ActiveRecordBuilder instance.
